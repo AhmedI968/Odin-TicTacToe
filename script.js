3dac2762e54e1c33e269eb36244cbd9faa9bb2a7
AhmedI968 <86612161+AhmedI968@users.noreply.github.com>
2024-08-07 07:16:45 -0600

    Created gameController function

--- script.js
+++ script.js
@@ -18,29 +18,56 @@ const gameBoard = (() => {
     };
 
     return {changeBoard, getBoard, resetBoard};
-});
+})();
 
 const displayController = (() => {
+    const board = document.querySelectorAll('.cell');
     const message = document.querySelector('.message');
-    const board = gameBoard();
+    const resetBtn = document.querySelector('.reset');
+})();
+
+
+const gameController = (() => {
     const player1 = Player('X');
-    const player2 = Player('O');
+    const player2 = Player('O'); 
     let currentPlayer = player1;
 
     const changePlayer = () => {
         currentPlayer = currentPlayer === player1 ? player2 : player1;
-        message.textContent = `Player ${currentPlayer.getSign()}'s turn`;
     };
 
-    const play = (index) => {
-        board.changeBoard(currentPlayer.getSign(), index);
-        changePlayer();
+    const checkWinner = (sign) => {
+        const winningCombos = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
+            [0, 4, 8], [2, 4, 6] // diagonal
+        ];
+
+        return winningCombos.some(combo => {
+            return combo.every(index => gameBoard.getBoard(index) === sign);
+        });
     };
 
-    const reset = () => {
-        board.resetBoard();
-        currentPlayer = player1;
+    const checkDraw = () => {
+        return gameBoard.getBoard().every(cell => cell !== '');
     };
 
-    return {play, reset};
-});
+    const playGame = (index) => {
+        if (gameBoard.getBoard(index) === '') {
+            gameBoard.changeBoard(currentPlayer.getSign(), index);
+            displayController.updateBoard(index, currentPlayer.getSign());
+
+            if (checkWinner(currentPlayer.getSign())) {
+                displayController.setMessage(`Player ${currentPlayer.getSign()} wins!`);
+                displayController.disableBoard();
+            } else if (checkDraw()) {
+                displayController.setMessage('It\'s a draw!');
+            } else {
+                changePlayer();
+            }
+        }
+    };
+
+    return {playGame};
+})();
+
